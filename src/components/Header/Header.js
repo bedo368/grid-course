b64449cfb557f89fe5dba601313ee79890fd9f5f
bedo <you@example.com>
2021-03-20 13:07:23 +0200

    done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react"
 import "./Header.scss"
+import Logo from "../../img/logo.png"
+import img1 from "../../img/logo-bbc.png"
+import img2 from "../../img/logo-bi.png"
+import img3 from "../../img/logo-forbes.png"
+import img4 from "../../img/logo-techcrunch.png"
 const Header = () => {
   return (
     <div className="header">
-      header
+      <img src={Logo} className="logo" />
+      <p>YOUR OWN HOME:</p>
+      <h2>The ultimate personal freedom</h2>
+      <button className="btn header-btn">view your profile</button>
+      <div className="seen-on">seen on</div>
+      <div className="header-imgs">
+        <img src={img1} alt="" className="header-img" />
+        <img src={img2} alt="" className="header-img" />
+        <img src={img3} alt="" className="header-img" />
+        <img src={img4} alt="" className="header-img" />
+      </div>
     </div>
   )
 }
--- src/components/RealTours/RealTours.js
+++ src/components/RealTours/RealTours.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react"
 import "./RealTours.scss"
+import rator1 from "../../img/realtor-1.jpeg"
+import rator2 from "../../img/realtor-2.jpeg"
+import rator3 from "../../img/realtor-3.jpeg"
 const RealTours = () => {
   return (
     <div className="realtours">
-      top 3 real tours
+      <p>top 3 real tours</p>
+      <div className="relators">
+        <div className="relator">
+          <img src={rator1} alt="" className="relator-img" />
+          <div className="info">
+            <div className="name">Toby Ramsey</div>
+            <p className="num-of-house">198 HOUSES SOLD</p>
+          </div>
+        </div>
+
+        <div className="relator">
+          <img src={rator2} alt="" className="relator-img" />
+          <div className="info">
+            <div className="name">Erik Feinman</div>
+            <p className="num-of-house">245 HOUSES SOLD</p>
+          </div>
+        </div>
+
+        <div className="relator">
+          <img src={rator3} alt="" className="relator-img" />
+          <div className="info">
+            <div className="name">Kim Brown</div>
+            <p className="num-of-house">212 HOUSES SOLD</p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
